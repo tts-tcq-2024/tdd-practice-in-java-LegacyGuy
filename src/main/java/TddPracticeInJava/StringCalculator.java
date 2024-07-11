5b1353fb11906d030ede9a2318048ada0cc1d16b
LegacyGuy <prathameshkashid26@gmail.com>
2024-07-11 18:26:45 +0530

    Update StringCalculator.java
     add method to check if the input is 0 and return 0 accordingly.

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -5,6 +5,9 @@ public class StringCalculator {
         if (input == null || input.isEmpty()) {
             return 0;
         }
+        if ("0".equals(input)) {
+            return 0;
+        }
         return 0;
     }
 }
