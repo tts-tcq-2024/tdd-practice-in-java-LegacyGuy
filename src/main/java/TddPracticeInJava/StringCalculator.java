d17ec630c860c7a5b4c0230d0101c336a9ee8242
LegacyGuy <prathameshkashid26@gmail.com>
2024-07-11 18:19:57 +0530

    Update StringCalculator.java

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -1,8 +1,11 @@
 package TddPracticeInJava;
-public class StringCalculator {
 
+public class StringCalculator {
     public int add(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
+        return 0;
     }
+}
+
