7bcc8eaeee503da4a99d30dde38f6365868508fa
LegacyGuy <prathameshkashid26@gmail.com>
2024-07-15 11:51:37 +0530

    Update StringCalculatorTest.java

--- src/test/java/TddPracticeInJava/StringCalculatorTest.java
+++ src/test/java/TddPracticeInJava/StringCalculatorTest.java
@@ -6,68 +6,84 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
-    
     @Test
-    public void ExpectZeroForEmptyInput()
-    {
+    public void ExpectZeroForEmptyInput() {
         int expectedResult = 0;
         String input = "";
         StringCalculator objUnderTest = new StringCalculator();
         int result = objUnderTest.add(input);
-
-       assertEquals(expectedResult,result);
+        assertEquals(expectedResult, result);
     }
 
-
-  @Test
-    public void ExpectZeroForSingleZero()
-    {
+    @Test
+    public void ExpectZeroForSingleZero() {
         int expectedResult = 0;
         String input = "0";
         StringCalculator objUnderTest = new StringCalculator();
         int result = objUnderTest.add(input);
-       assertEquals(expectedResult,result);
-
+        assertEquals(expectedResult, result);
     }
 
-   @Test
-    public void ExpectSumForTwoNumbers()
-    {
+    @Test
+    public void ExpectSumForTwoNumbers() {
         int expectedResult = 3;
         String input = "1,2";
         StringCalculator objUnderTest = new StringCalculator();
         int result = objUnderTest.add(input);
-        assertEquals(expectedResult,result);
+        assertEquals(expectedResult, result);
     }
 
-   @Test
-    public void ExpectSumWithNewlineDelimiter()
-    {
+    @Test
+    public void ExpectSumWithNewlineDelimiter() {
         int expectedResult = 6;
         String input = "1\n2,3";
         StringCalculator objUnderTest = new StringCalculator();
         int result = objUnderTest.add(input);
-         assertEquals(expectedResult,result);
+        assertEquals(expectedResult, result);
     }
 
- 
     @Test
-    public void ignoreNumbersGreaterThan1000() {
+    public void IgnoreNumbersGreaterThan1000() {
         int expectedResult = 1;
         String input = "1,1001";
         StringCalculator objUnderTest = new StringCalculator();
         int result = objUnderTest.add(input);
-
-       assertEquals(expectedResult,result);
+        assertEquals(expectedResult, result);
     }
+
     @Test
-    public void ExpectSumWithCustomDelimiter()
-    {
+    public void ExpectSumWithCustomDelimiter() {
         int expectedResult = 3;
         String input = "//;\n1;2";
         StringCalculator objUnderTest = new StringCalculator();
         int result = objUnderTest.add(input);
-
-      assertEquals(expectedResult,result);
+        assertEquals(expectedResult, result);
     }
- }
+
+    @Test
+    public void ExpectSumWithMultipleCustomDelimiters() {
+        int expectedResult = 6;
+        String input = "//[***][%%%]\n1***2%%%3";
+        StringCalculator objUnderTest = new StringCalculator();
+        int result = objUnderTest.add(input);
+        assertEquals(expectedResult, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ThrowExceptionForNegativeNumbers() {
+        String input = "1,-2,3";
+        StringCalculator objUnderTest = new StringCalculator();
+        objUnderTest.add(input);
+    }
+
+    @Test
+    public void ThrowExceptionForMultipleNegativeNumbers() {
+        String input = "-1,-2,3";
+        StringCalculator objUnderTest = new StringCalculator();
+        try {
+            objUnderTest.add(input);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Negatives not allowed: [-1, -2]", e.getMessage());
+        }
+    }
+}
